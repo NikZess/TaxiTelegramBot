1c5aeb292e231c30e99544fa3ef58ffa91bbd891
Dmitry <157917126+NikZess@users.noreply.github.com>
2024-11-08 23:33:06 +0300

    A taxi bot is needed to order a taxi quickly. The main application has been created.

---
+++ app.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+ALLOWED_UPDATES = ['message, edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+
+dp = Dispatcher()
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+asyncio.run(main())
